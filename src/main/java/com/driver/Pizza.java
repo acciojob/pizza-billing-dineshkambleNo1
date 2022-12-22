ad61bd3f46ad71bdae371c092e3fda64a3d83411
dinesh <dineshrock153@gmail.com>
2022-12-22 11:54:13 +0530

    check1

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,13 +2,51 @@ package com.driver;
 
 public class Pizza {
 
-    private int price;
+    private int price; // final bill price
     private Boolean isVeg;
     private String bill;
+
+    private  int cheese_price;
+
+    private int toppings_price;
+
+    private int paperBag_price;
+
+    //for check
+
+    boolean isCheeseAdded;
+    boolean isToppingAdded;
+
+    boolean isBagPriceAdded;
+
+
+    boolean isbillgenerated;
+
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+
+        this.cheese_price = 80;
+        this.paperBag_price = 20;
+
+        if(isVeg) {
+
+            this.price = 300;
+            this.toppings_price = 70;
+        }
+        else{
+            this.price = 400;
+            this.toppings_price = 120;
+        }
+
+        isCheeseAdded = false;
+        isToppingAdded = false;
+        isBagPriceAdded = false;
+
+        this.bill = "Base Price Of The Pizza:"+this.price + "\n";
+
     }
 
     public int getPrice(){
@@ -17,18 +55,74 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+
+        if(!isCheeseAdded)
+        {
+            this.price = this.price + this.cheese_price;
+
+            // or fir cheese ko add kardo
+
+            isCheeseAdded = true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+
+        if(!isToppingAdded)
+        {
+            this.price = this.price + this.toppings_price;
+
+            // or fir cheese ko add kardo
+
+            isToppingAdded  = true;
+        }
+
     }
 
     public void addTakeaway(){
         // your code goes here
+
+
+
+        if(!isBagPriceAdded)
+        {
+            this.price = this.price + this.paperBag_price;
+
+            // or fir cheese ko add kardo
+
+            isBagPriceAdded  = true;
+        }
     }
+
+    //
+//         80
+//         70
+//         20
+//         470
 
     public String getBill(){
         // your code goes here
+        if(!isbillgenerated){
+
+            if(isCheeseAdded)
+            {
+                this.bill = this.bill +  "Extra Cheese Added: "+ this.cheese_price + "\n";
+            }
+
+            if(isToppingAdded){
+                this.bill = this.bill +"Extra Toppings Added: "+ this.toppings_price + "\n";
+            }
+
+            if(isBagPriceAdded){
+                this.bill = this.bill +"Paperbag Added: "+  this.paperBag_price + "\n";
+            }
+
+            this.bill = this.bill + "Total Price: "+ this.price;
+
+            isbillgenerated = true; // bhai don't generate bill again and again
+
+        }
         return this.bill;
     }
 }
